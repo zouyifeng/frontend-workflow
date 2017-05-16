23c52ab66daaebacd6de7262c1da9c560c2ee715
zouyifeng <zouyifeng@kuaiyd.com>
2017-05-16 09:27:50 +0800

    init-webpack-01

---
+++ app/greeter.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import config from './config.json'
+
+import styles from './greeter.css'
+
+class Greeter extends Component {
+    render() {
+        return (
+            <div className={styles.root}>
+                hello112
+            </div>
+        );
+    }
+}
+
+export default Greeter
